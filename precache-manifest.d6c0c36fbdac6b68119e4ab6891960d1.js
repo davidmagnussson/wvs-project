9392ffd3d5b348a458c7f5b005e625eb07995952
davidmagnussson <davidmagnusson97@gmail.com>
2020-02-23 11:50:12 +0100

    Updates

---
+++ precache-manifest.d6c0c36fbdac6b68119e4ab6891960d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd836d3a0b1bd4f1b2e6ec73c4647710",
+    "url": "/wvs-project/index.html"
+  },
+  {
+    "revision": "f6710e6e563d7aadf355",
+    "url": "/wvs-project/static/css/main.65a7f845.chunk.css"
+  },
+  {
+    "revision": "714557c981853ca27c31",
+    "url": "/wvs-project/static/js/2.816a2535.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/wvs-project/static/js/2.816a2535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6710e6e563d7aadf355",
+    "url": "/wvs-project/static/js/main.326d43b0.chunk.js"
+  },
+  {
+    "revision": "4c297727e96648f3d9d7",
+    "url": "/wvs-project/static/js/runtime-main.29309e56.js"
+  }
+]);
